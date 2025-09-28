1d257c909d43adac8927593122905481925c08e5
minju.H <105367622+minjuuH@users.noreply.github.com>
2025-09-28 15:50:53 +0900

    chore: 로그인 페이지 수정
    login_page.dart
    Stateless에서 Stateful로 변경

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,20 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/signup_page.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+
+}
+
+class _LoginPageState extends State<LoginPage> {
+  //컨트롤러
+  final TextEditingController _idController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    //메모리 누수 방지
+    _idController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _logIn() {
+    final id = _idController.text.trim();
+    final password = _passwordController.text.trim();
+
+    if (password == " " && id ==" ") {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("아이디와 비밀번호가 일치하지 않습니다."))
+      );
+      return;
+    }
+
+    // TODO: 로그인 로직 구현
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text("로그인 시도"))
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("로그인"),
-      ),
-      body: const Center(
-        child: Text("login test"),
+      appBar: AppBar(title: const Text("로그인"),),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(height: 40,),
+            TextField(
+              controller: _idController,
+              decoration: const InputDecoration(
+                labelText: "id",
+                border: OutlineInputBorder()
+              ),
+              keyboardType: TextInputType.text,
+            ),
+            const SizedBox(height: 16,),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                labelText: "비밀번호",
+                border: OutlineInputBorder()
+              ),
+              obscureText: true,  //텍스트 가림 처리
+            ),
+            const SizedBox(height: 24,),
+            
+            ElevatedButton(onPressed: _logIn, child: const Text("로그인")),
+            
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context, 
+                  MaterialPageRoute(builder: (context)=>const SignupPage()),
+                );
+              }, 
+              child: const Text("회원가입")),
+          ],
+        ),
       ),
     );
-
-    throw UnimplementedError();
   }
+
 }
