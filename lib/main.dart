60b6fe07985e9c530168db68207c025dac53840d
minju.H <105367622+minjuuH@users.noreply.github.com>
2025-10-13 09:35:01 +0900

    feat: main 화면 하단 네비게이터바 추가
    도우미, 도서관, 홈, 책장, 프로필의 5개 탭으로 구성

--- lib/main.dart
+++ lib/main.dart
@@ -13,24 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'book2002',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 243, 245, 241)),
       ),
-      home: const MyHomePage(title: 'Home Page'),
+      home: const MyHomePage(title: ''),
     );
   }
 }
@@ -54,82 +39,86 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _selectedIndex = 2;   //하단 네비게이터 탭 인덱스, 중앙의 홈을 디폴트로 설정
 
-  void _incrementCounter() {
+  //탭 클릭 시 호출되는 함수
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255), //배경색 지정
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        backgroundColor: Colors.white,
         title: Text(widget.title),
         actions: [
-          //로그인 버튼
-          TextButton(
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>const LoginPage()),);
-              //Navigator -> 로그인 화면 전환
-            },
-            child: const Text(
-              "로그인",
-              style: TextStyle(color: Colors.black),
+          Container(
+            margin: const EdgeInsets.only(right: 12, top: 6, bottom: 6),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                side: const BorderSide(color: Color.fromARGB(255, 190, 190, 190), width: 1),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),     //둥근 모서리
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),  //안쪽 여백 설정
+              ),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>const LoginPage()),);
+                //Navigator -> 로그인 화면 전환
+              },
+              child: const Text(
+                "로그인",
+                style: TextStyle(color: Color.fromARGB(255, 110, 110, 110)),
+              ),
             ),
-          )
+          ),
+          //로그인 버튼
+          
         ],
       ),
+      //메인 화면 내용
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+        child: IndexedStack(
+          index: _selectedIndex,
+          children: const [
+            //추후 각 화면 위젯화하는 작업 필요
+            const Text("도우미"),
+            const Text("도서관"),
+            const Text("홈"),
+            const Text("책장"),
+            const Text("프로필"),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+      //하단 네비게이터바
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        currentIndex: _selectedIndex,     //인덱스 지정
+        onTap: _onItemTapped,
+
+        //레이블 이름 표시 끔
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+
+        selectedItemColor: const Color.fromARGB(255, 18, 110, 21),
+        unselectedItemColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.help), label: '도우미'),
+          BottomNavigationBarItem(icon: Icon(Icons.map), label: '도서관'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
+          BottomNavigationBarItem(icon: Icon(Icons.book), label: '책장'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: '프로필'),
+        ],
+      ),
+      
     );
   }
 }
