c9d218df489237e3ae20cc0b09a47b3e422f7669
minju.H <105367622+minjuuH@users.noreply.github.com>
2025-11-12 01:42:37 +0900

    style: 상수 파일 분리

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const String signupApiUrl = "http://localhost:8080/api/v1/member/signup";
+const Color primaryColor = Color(0xFF00AA00);
+const Color errorColor = Color(0xFFCC0000);
