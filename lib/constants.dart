029ecf441d7a9be4692660e77c3e91a37ccda352
minju.H <105367622+minjuuH@users.noreply.github.com>
2025-12-02 18:22:22 +0900

    build: 서버 주소 연결
    *웹 환경은 security 문제로 정상 작동 x*

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,14 +2,13 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 
-// final String baseUrl = Platform.isAndroid ? "http://10.0.2.2:8080" : "http://localhost:8080";
-final String baseUrl = "http://localhost:8080";
-const String signupApiUrl = "http://localhost:8080/api/v1/member/signup";
-const String loginApiUrl = "http://localhost:8080/api/v1/member/login";
-const String googleApiUrl = "http://localhost:8080/oauth2/authorization/google";
-const String profileSetupApiUrl = "http://localhost:8080/api/v1/profile/create";
-const String profileEditApiUrl = "http://localhost:8080/api/v1/profile/update";
-const String profileApiUrl = "http://localhost:8080/api/v1/profile/";
+final String baseUrl = "http://43.200.103.186:8080";
+const String signupApiUrl = "http://43.200.103.186:8080/api/v1/member/signup";
+const String loginApiUrl = "http://43.200.103.186:8080/api/v1/member/login";
+const String googleApiUrl = "http://43.200.103.186:8080/oauth2/authorization/google";
+const String profileSetupApiUrl = "http://43.200.103.186:8080/api/v1/profile/create";
+const String profileEditApiUrl = "http://43.200.103.186:8080/api/v1/profile/update";
+const String profileApiUrl = "http://43.200.103.186:8080/api/v1/profile/";
 
 const Color primaryColor = Color(0xFF00AA00);
 const Color errorColor = Color(0xFFCC0000);
